43358794d83e101d7dc25370362c3af5a807087b
Francisco Cabezas <fcabezas.berrios@gmail.com>
2020-01-02 16:00:48 -0300

    Added MongoDB support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import sms from './v1/sms/index';
 
@@ -18,6 +19,10 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+//Connect to the MongoDB database  
+mongoose.connect('mongodb://' + process.env.MONGO_HOST + '/' + process.env.MONGO_INITDB_DATABASE, 
+            { useNewUrlParser: true, useUnifiedTopology: true });  
 
 app.use('/v1/sms', sms);
 
