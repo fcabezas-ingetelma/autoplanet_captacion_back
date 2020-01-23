a6824e0edfea6dd7760d056bc6892ad2db6144f0
Francisco Cabezas <fcabezas.berrios@gmail.com>
2020-01-23 17:16:56 -0300

    Setup HTTPS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 
 import sms from './v1/routes/sms/index';
 import user from './v1/routes/user/index';
@@ -15,13 +17,15 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-//Development
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "*");
-    next();
-});
+if(process.env.NODE_ENV == 'development') {
+    //Development
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "*");
+        next();
+    });
+}
 
 app.use('/v1/sms', sms);
 app.use('/v1/user', user);
@@ -32,6 +36,9 @@ app.get('/', (req, res) => {
     res.send('Index');
 })
 
-app.listen(process.env.PORT, () => {
-    console.log('Example app listening on port ' + process.env.PORT);
-})
+https.createServer({
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem'),
+    passphrase: process.env.PEM_PASSPHRASE
+}, app)
+.listen(process.env.PORT);
